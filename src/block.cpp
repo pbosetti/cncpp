3cfa1523e5b8e01d78bf611902e1e252f4b47a65
Paolo Bosetti <paolo.bosetti@unitn.it>
2026-04-16 13:24:20 +0200

    Lesson 20260416

---
+++ src/block.cpp
@@ -0,0 +1,142 @@
+/*
+  ____  _            _           _               
+ | __ )| | ___   ___| | __   ___| | __ _ ___ ___ 
+ |  _ \| |/ _ \ / __| |/ /  / __| |/ _` / __/ __|
+ | |_) | | (_) | (__|   <  | (__| | (_| \__ \__ \
+ |____/|_|\___/ \___|_|\_\  \___|_|\__,_|___/___/
+                                                 
+Block class implementation
+ */
+
+#include "block.hpp"
+#include <iostream>
+#include <sstream>
+
+ using namespace std;
+ using namespace cncpp;
+
+
+ // LIFECYCLE
+Block::Block(string line) : _line(line), _n(0) {}
+
+Block::Block(string line, Block &prev) : Block(line) {
+  *this = prev;
+}
+
+Block::~Block() {
+  cerr << "Block " << _line << " destroyed." << endl;
+}
+
+std::string Block::desc(bool colored = true) {
+
+}
+
+Block &Block::operator=(Block &b) {
+  if (!b._parsed) throw runtime_error("Previous block has not been parsed");
+  _tool = b._tool;
+  _feedrate = b._feedrate;
+  _spindle = b._spindle;
+  _n = b._n + 1;
+  _target.reset();
+  prev = &b;
+  b.next = this;
+}
+
+
+
+ // OPERATIONS/OPERATORS
+
+Block &Block::parse(const Machine *m) {
+  _machine = m;
+  stringstream ss(_line);
+  string token;
+
+  while ( ss >> token ) {
+    try {
+      if (!parse_token(token)) break;
+    } catch (runtime_error &e) {
+      stringstream es;
+      es << "Parsing error at line: " << _line << endl;
+      es << "Token: " << token << endl;
+      es << "Exception: " << e.what() << endl;
+      throw runtime_error(es.str());
+    }
+  }
+  // Modal behavior
+  _target.modal(prev->target());
+  _delta = _target.delta(prev->target());
+  // TODO: set _acc
+  _length = _delta.length();
+
+  // Calculate profile
+  switch (_type)
+  {
+  case BlockType::LINE:
+    // calculate segment profile
+    break;
+  
+  case BlockType::CWA:
+  case BlockType::CCWA:
+    // calculate arc profile
+    break;
+
+  default:
+    break;
+  }
+
+  _parsed = true;
+  return *this;
+}
+
+data_t Block::lambda(data_t time, data_t &speed) {
+  if (!_parsed) throw runtime_error("Block not parsed");
+  return _profile.lambda(time, speed);
+}
+
+Point Block::interpolate(data_t lambda) {
+  if (!_parsed) throw runtime_error("Block not parsed");
+  Point result = Point();
+
+  // Interpolation:
+  // x(t) = x(0) + d_x * lambda(t)
+  // y(t) = y(0) + d_y * lambda(t)
+  // z(t) = z(0) + d_z * lambda(t)
+
+  if (_type == BlockType::LINE) {
+    result.x(prev->target().x() + _delta.x() * lambda);
+    result.y(prev->target().y() + _delta.y() * lambda);
+  } else if ( _type == BlockType::CWA || _type == BlockType::CCWA ) {
+    
+  } else {
+    throw runtime_error("Cannot interpolate block" + _line);
+  }
+  result.z(prev->target().z() + _delta.z() * lambda);
+
+
+  return result;
+}
+
+Point Block::interpolate(data_t time, data_t &lambda, data_t &speed) {
+
+}
+
+void Block::walk(std::function<void(Block &b, data_t t, data_t l, data_t s)> func) {
+
+}
+
+
+
+
+ // ACCESSORS
+
+
+
+
+
+ // PRIVATE METHODS
+
+bool Block::parse_token(string &token) {
+  bool res = true;
+
+  return res;
+}
---
+++ src/block.hpp
@@ -0,0 +1,103 @@
+/*
+  ____  _            _           _               
+ | __ )| | ___   ___| | __   ___| | __ _ ___ ___ 
+ |  _ \| |/ _ \ / __| |/ /  / __| |/ _` / __/ __|
+ | |_) | | (_) | (__|   <  | (__| | (_| \__ \__ \
+ |____/|_|\___/ \___|_|\_\  \___|_|\__,_|___/___/
+                                                 
+
+Implements a G-code block, which is a collection of G-code commands that are executed together. Each block can contain multiple commands, and each command can have multiple parameters.
+*/
+
+#pragma once
+
+#include "defines.hpp"
+#include "point.hpp"
+#include <functional>
+
+namespace cncpp {
+
+  class Machine {
+
+  };
+
+class Block {
+public:
+
+  enum class BlockType {
+    RAPID = 0,
+    LINE,
+    CWA,
+    CCWA,
+    NO_MOTION
+  };
+
+  struct Profile {
+    data_t a, d;
+    data_t f, l;
+    data_t fs, fe;
+    data_t dt_1, dt_m, dt_2;
+    data_t dt;
+    data_t current_acc; // along an arc
+    data_t lambda(data_t t, data_t &s);
+  };
+
+  // LIFECYCLE
+  Block(std::string line);
+  Block(std::string line, Block &prev);
+  ~Block();
+  std::string desc(bool colored = true);
+  Block &operator=(Block &b);
+
+  // OPERATIONS/OPERATORS
+  Block &parse(const Machine *m);
+  data_t lambda(data_t time, data_t &speed);
+  Point interpolate(data_t lambda);
+  Point interpolate(data_t time, data_t &lambda, data_t &speed);
+  void walk(std::function<void(Block &b, data_t t, data_t l, data_t s)> func);
+
+  // ACCESSORS
+  std::string line() const { return _line; }
+  size_t n() const { return _n; }
+  data_t dt() const { return _profile.dt; }
+  BlockType type() const { return _type; }
+  size_t tool() const { return _tool; }
+  data_t feedrate() const { return _feedrate; }
+  data_t arc_feedrate() const { return _arc_feedrate; }
+  data_t spindle() const { return _spindle; }
+  data_t length() const { return _length; }
+  const Point &target() const { return _target; }
+  const Point &center() const { return _center; }
+  const Point &delta() const { return _delta; }
+  size_t m() const { return _m; }
+  const Profile &profile() const { return _profile; }
+  bool parsed() const { return _parsed; }
+
+  Block *prev = nullptr;
+  Block *next = nullptr;
+
+private:
+  std::string _line; // original G-Code line, eg "N01 G00 X100"
+  size_t _n = 0;     // block number
+  Point _target = Point();
+  Point _center = Point();
+  Point _delta = Point();
+  data_t _length = 0;
+  data_t _i = 0, _j = 0, _r = 0; // arc parameters
+  data_t _theta_0 = 0, _dtheta = 0;
+  data_t _feedrate = 0; // feedrate
+  data_t _arc_feedrate = 0; // feedrate along the arc
+  data_t _spindle = 0; // spindle rate
+  data_t _acc = 0; // actual acceleration
+  size_t _m = 0; // M command argument
+  size_t _tool = 0; // current tool number
+  BlockType _type = BlockType::NO_MOTION;
+  Profile _profile;
+  bool _parsed = false;
+  Machine const *_machine = nullptr;
+
+  bool parse_token(string &token)
+
+}; // class Block
+
+} // namespace cncpp
